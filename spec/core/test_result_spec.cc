4d6956b43ac2e42ddd9d56890031379bc5b80656
horance <horance@outlook.com>
2019-09-19 16:23:08 +0800

    replcace gtest with cctest for test result spec

--- spec/core/test_result_spec.cc
+++ spec/core/test_result_spec.cc
@@ -1,7 +1,5 @@
-#include "gtest/gtest.h"
-#include "cctest/except/assertion_error.h"
+#include "cctest/cctest.h"
 #include "cctest/core/test_result.h"
-#include "cctest/core/test_case.h"
 #include "cctest/listener/collector/failure_list.h"
 #include "cctest/listener/collector/test_collector.h"
 
@@ -9,141 +7,139 @@ using namespace cctest;
 
 namespace {
 
-struct TestCaseSpec : testing::Test {
-protected:
-  void run(::Test& test) {
-    test.run(result);
-  }
+FIXTURE(TestCaseSpec) {
+  FailureList list;
+  TestCollector collector;
+  TestResult result;
 
-private:
-  void SetUp() override {
+  SETUP {
     result.addListener(collector);
     result.addListener(list);
   }
 
-protected:
-  FailureList list;
-  TestCollector collector;
-  TestResult result;
+  void run(::Test& test) {
+    test.run(result);
+  }
+
+  struct FailureOnRunningTest : TestCase {
+    const char* expectMsg() const {
+      return "assertion fail in the runTest\n"
+              "product.cc:57\n"
+              "expected value == 2, but got 3";
+    }
+
+  private:
+    void runTest() override {
+      throw AssertionError("product.cc:57", "expected value == 2, but got 3");
+    }
+  };
+
+  TEST("throw assertion error on run test") {
+    FailureOnRunningTest test;
+    run(test);
+
+    ASSERT_EQ(1, collector.failCount());
+  }
+
+  TEST("extract except msg on running test failed") {
+    FailureOnRunningTest test;
+    run(test);
+
+    list.foreach([&test](const TestFailure& fail){
+      ASSERT_TRUE(fail.isFailure());
+      ASSERT_EQ(test.expectMsg(), fail.getExceptionMsg());
+    });
+  }
+
+  struct FailureOnSetUp : TestCase {
+    bool wasRun = false;
+
+  private:
+    void setUp() override {
+      throw AssertionError("product.cc:57", "expected value == 2, but got 3");
+    }
+
+    void runTest() override {
+      wasRun = true;
+    }
+  };
+
+  TEST("throw assertion error on setup") {
+    FailureOnSetUp test;
+    run(test);
+
+    ASSERT_EQ(1, collector.failCount());
+    ASSERT_FALSE(test.wasRun);
+  }
+
+  struct FailureOnTearDown : TestCase {
+    void tearDown() override {
+      throw AssertionError("product.cc:57", "expected value == 2, but got 3");
+    }
+  };
+
+  TEST("throw assertion error on tear down") {
+    FailureOnTearDown test;
+    run(test);
+    ASSERT_EQ(1, collector.failCount());
+  }
+
+  struct ErrorOnRunningTest : TestCase {
+    const char* expectMsg() const {
+      return "uncaught std::exception in the runTest\n"
+              "std::exception";
+    }
+
+    void runTest() override {
+      throw std::exception();
+    }
+  };
+
+  TEST("throw std exception on run test") {
+    ErrorOnRunningTest test;
+    run(test);
+    ASSERT_EQ(1, collector.errorCount());
+  }
+
+  TEST("extract error msgt on running test failed") {
+    ErrorOnRunningTest test;
+    run(test);
+
+    list.foreach([&test](const TestFailure& error){
+      ASSERT_FALSE(error.isFailure());
+      ASSERT_EQ(test.expectMsg(), error.getExceptionMsg());
+    });
+  }
+
+  struct NilException {};
+
+  struct UnknownErrorOnRunningTest : TestCase {
+    const char* expectMsg() const {
+      return "uncaught unknown exception in the runTest\n";
+    }
+
+  private:
+    void runTest() override {
+      throw NilException();
+    }
+  };
+
+  TEST("throw unknown exception on running test") {
+    UnknownErrorOnRunningTest test;
+    run(test);
+    ASSERT_EQ(1, collector.errorCount());
+  }
+
+  TEST("extract unknown error msg on running test failed") {
+    UnknownErrorOnRunningTest test;
+    run(test);
+
+    list.foreach([&test](const TestFailure& error){
+      ASSERT_FALSE(error.isFailure());
+      ASSERT_EQ(test.expectMsg(), error.getExceptionMsg());
+    });
+  }
+
 };
-
-struct FailureOnRunningTest : TestCase {
-  const char* expectMsg() const {
-    return "assertion fail in the runTest\n"
-            "product.cc:57\n"
-            "expected value == 2, but got 3";
-  }
-
-private:
-  void runTest() override {
-    throw AssertionError("product.cc:57", "expected value == 2, but got 3");
-  }
-};
-
-TEST_F(TestCaseSpec, throw_assertion_error_on_run_test) {
-  FailureOnRunningTest test;
-  run(test);
-
-  ASSERT_EQ(1, collector.failCount());
-}
-
-TEST_F(TestCaseSpec, extract_except_msg_on_running_test_failed) {
-  FailureOnRunningTest test;
-  run(test);
-
-  list.foreach([&test](const TestFailure& fail){
-    ASSERT_TRUE(fail.isFailure());
-    ASSERT_EQ(test.expectMsg(), fail.getExceptionMsg());
-  });
-}
-
-struct FailureOnSetUp : TestCase {
-  bool wasRun = false;
-
-private:
-  void setUp() override {
-    throw AssertionError("product.cc:57", "expected value == 2, but got 3");
-  }
-
-  void runTest() override {
-    wasRun = true;
-  }
-};
-
-TEST_F(TestCaseSpec, throw_assertion_error_on_setup) {
-  FailureOnSetUp test;
-  run(test);
-
-  ASSERT_EQ(1, collector.failCount());
-  ASSERT_FALSE(test.wasRun);
-}
-
-struct FailureOnTearDown : TestCase {
-  void tearDown() override {
-    throw AssertionError("product.cc:57", "expected value == 2, but got 3");
-  }
-};
-
-TEST_F(TestCaseSpec, throw_assertion_error_on_tear_down) {
-  FailureOnTearDown test;
-  run(test);
-  ASSERT_EQ(1, collector.failCount());
-}
-
-struct ErrorOnRunningTest : TestCase {
-  const char* expectMsg() const {
-    return "uncaught std::exception in the runTest\n"
-            "std::exception";
-  }
-
-  void runTest() override {
-    throw std::exception();
-  }
-};
-
-TEST_F(TestCaseSpec, throw_std_exception_on_run_test) {
-  ErrorOnRunningTest test;
-  run(test);
-  ASSERT_EQ(1, collector.errorCount());
-}
-
-TEST_F(TestCaseSpec, extract_error_msgt_on_running_test_failed) {
-  ErrorOnRunningTest test;
-  run(test);
-
-  list.foreach([&test](const TestFailure& error){
-    ASSERT_FALSE(error.isFailure());
-    ASSERT_EQ(test.expectMsg(), error.getExceptionMsg());
-  });
-}
-
-struct NilException {};
-
-struct UnknownErrorOnRunningTest : TestCase {
-  const char* expectMsg() const {
-    return "uncaught unknown exception in the runTest\n";
-  }
-
-private:
-  void runTest() override {
-    throw NilException();
-  }
-};
-
-TEST_F(TestCaseSpec, throw_unknown_exception_on_running_test) {
-  UnknownErrorOnRunningTest test;
-  run(test);
-  ASSERT_EQ(1, collector.errorCount());
-}
-
-TEST_F(TestCaseSpec, extract_unknown_error_msg_on_running_test_failed) {
-  UnknownErrorOnRunningTest test;
-  run(test);
-
-  list.foreach([&test](const TestFailure& error){
-    ASSERT_FALSE(error.isFailure());
-    ASSERT_EQ(test.expectMsg(), error.getExceptionMsg());
-  });
-}
 
 } // namespace
