6eab47f3de98e26ccebdd724a33c20a25b28399f
horance <horance@outlook.com>
2019-09-19 16:40:33 +0800

    replcace gtest with cctest for test status spec

--- spec/listener/test_status_spec.cc
+++ spec/listener/test_status_spec.cc
@@ -1,46 +1,41 @@
+#include "cctest/cctest.h"
 #include "cctest/listener/collector/test_status.h"
 #include "cctest/core/test_result.h"
-#include "cctest/core/test_case.h"
-#include "cctest/except/assertion_error.h"
-#include "gtest/gtest.h"
 
 using namespace cctest;
 
 namespace {
 
-struct TestStatusSpec : testing::Test {
-protected:
+FIXTURE(TestStatusSpec) {
+  TestResult result;
+  TestStatus status;
+
+  SETUP {
+    result.addListener(status);
+  }
+
   void run(cctest::Test& test) {
     test.run(result);
   }
 
-private:
-  void SetUp() override {
-    result.addListener(status);
+  TEST("should be successful") {
+    TestCase test;
+    run(test);
+    ASSERT_TRUE(status.successful());
   }
 
-protected:
-  TestResult result;
-  TestStatus status;
-};
+  struct FailureOnRunningTest : TestCase {
+  private:
+    void runTest() override {
+      throw AssertionError("product.cc:57", "expected value == 2, but got 3");
+    }
+  };
 
-TEST_F(TestStatusSpec, should_be_successful) {
-  TestCase test;
-  run(test);
-  ASSERT_TRUE(status.successful());
-}
-
-struct FailureOnRunningTest : TestCase {
-private:
-  void runTest() override {
-    throw AssertionError("product.cc:57", "expected value == 2, but got 3");
+  TEST("should be failed") {
+    FailureOnRunningTest test;
+    run(test);
+    ASSERT_FALSE(status.successful());
   }
 };
-
-TEST_F(TestStatusSpec, should_be_failed) {
-  FailureOnRunningTest test;
-  run(test);
-  ASSERT_FALSE(status.successful());
-}
 
 } // namespace
