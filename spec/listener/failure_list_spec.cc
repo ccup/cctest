6d7e17b1e2abce2031462f47b64b93740ee32128
horance <horance@outlook.com>
2019-09-19 16:34:47 +0800

    replcace gtest with cctest for failture list spec

--- spec/listener/failure_list_spec.cc
+++ spec/listener/failure_list_spec.cc
@@ -1,50 +1,45 @@
-#include "gtest/gtest.h"
-#include "cctest/core/test_case.h"
+#include "cctest/cctest.h"
 #include "cctest/core/test_result.h"
-#include "cctest/except/assertion_error.h"
 #include "cctest/listener/collector/failure_list.h"
 
 using namespace cctest;
 
 namespace {
 
-struct FailureListerSpec : testing::Test {
-protected:
-  void run(cctest::Test& test) {
-    test.run(result);
-  }
+FIXTURE(FailureListerSpec) {
+  FailureList list;
+  TestResult result;
 
-private:
-  void SetUp() override {
+  SETUP {
     result.addListener(list);
   }
 
-protected:
-  FailureList list;
-  TestResult result;
-};
+  void run(Test& test) {
+    test.run(result);
+  }
 
-struct FailureOnRunningTest : TestCase {
+  struct FailureOnRunningTest : TestCase {
   const char* expectMsg() const {
     return "assertion fail in the runTest\n"
             "product.cc:57\n"
             "expected value == 2, but got 3";
   }
 
-private:
-  void runTest() override {
-    throw AssertionError("product.cc:57", "expected value == 2, but got 3");
+  private:
+    void runTest() override {
+      throw AssertionError("product.cc:57", "expected value == 2, but got 3");
+    }
+  };
+
+  TEST("extract except msg on running test failed") {
+    FailureOnRunningTest test;
+    run(test);
+
+    list.foreach([&test](const TestFailure& fail) {
+      ASSERT_TRUE(fail.isFailure());
+      ASSERT_EQ(test.expectMsg(), fail.getExceptionMsg());
+    });
   }
 };
-
-TEST_F(FailureListerSpec, extract_except_msg_on_running_test_failed) {
-  FailureOnRunningTest test;
-  run(test);
-
-  list.foreach([&test](const TestFailure& fail) {
-    ASSERT_TRUE(fail.isFailure());
-    ASSERT_EQ(test.expectMsg(), fail.getExceptionMsg());
-  });
-}
 
 } // namespace
