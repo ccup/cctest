d790888a00009c76b2681c978361b706162cd2fa
horance <horance@outlook.com>
2019-09-19 16:43:48 +0800

    replcace gtest with cctest for text progress spec

--- spec/listener/text_progress_spec.cc
+++ spec/listener/text_progress_spec.cc
@@ -1,17 +1,18 @@
-#include "cctest/listener/text/text_progress.h"
+#include "cctest/cctest.h"
 #include "cctest/core/test_result.h"
-#include "cctest/core/test_case.h"
 #include "cctest/core/test_suite.h"
-#include "cctest/except/assertion_error.h"
-#include "gtest/gtest.h"
+#include "cctest/listener/text/text_progress.h"
 #include <sstream>
 
 using namespace cctest;
 
 namespace {
 
-struct TextProgressSpec : testing::Test {
-protected:
+FIXTURE(TextProgressSpec) {
+  TestResult result;
+  std::ostringstream ss;
+  TextProgress progress;
+
   TextProgressSpec() : progress(ss) {
     result.addListener(progress);
   }
@@ -24,58 +25,51 @@ protected:
     ASSERT_EQ(ss.str(), output);
   }
 
-protected:
-  TestResult result;
-  std::ostringstream ss;
-  TextProgress progress;
-};
+  TEST("single test case is successful") {
+    TestCase test;
+    run(test);
+    assertOutput("starting...\n*\nend.\n");
+  }
 
-TEST_F(TextProgressSpec, single_test_case_is_successful) {
-  TestCase test;
-  run(test);
-  assertOutput("starting...\n*\nend.\n");
-}
+  TEST("all of test cases are successful") {
+    TestSuite suite;
+    suite.add(new TestCase);
+    suite.add(new TestCase);
 
-TEST_F(TextProgressSpec, all_of_test_cases_are_successful) {
-  TestSuite suite;
-  suite.add(new TestCase);
-  suite.add(new TestCase);
+    run(suite);
+    assertOutput("starting...\n**\nend.\n");
+  }
 
-  run(suite);
-  assertOutput("starting...\n**\nend.\n");
-}
-
-namespace {
   struct FailureOnRunningTest : TestCase {
   private:
     void runTest() override {
       throw AssertionError("product.cc:57", "expected value == 2, but got 3");
     }
   };
-}
 
-TEST_F(TextProgressSpec, contains_failed_test_case) {
-  TestSuite suite;
-  suite.add(new TestCase);
-  suite.add(new FailureOnRunningTest);
+  TEST("contains failed test case") {
+    TestSuite suite;
+    suite.add(new TestCase);
+    suite.add(new FailureOnRunningTest);
 
-  run(suite);
-  assertOutput("starting...\n**F\nend.\n");
-}
+    run(suite);
+    assertOutput("starting...\n**F\nend.\n");
+  }
 
-struct ErrorOnRunningTest : TestCase {
-  void runTest() override {
-    throw std::exception();
+  struct ErrorOnRunningTest : TestCase {
+    void runTest() override {
+      throw std::exception();
+    }
+  };
+
+  TEST("contains error test case") {
+    TestSuite suite;
+    suite.add(new TestCase);
+    suite.add(new ErrorOnRunningTest);
+
+    run(suite);
+    assertOutput("starting...\n**E\nend.\n");
   }
 };
-
-TEST_F(TextProgressSpec, contains_error_test_case) {
-  TestSuite suite;
-  suite.add(new TestCase);
-  suite.add(new ErrorOnRunningTest);
-
-  run(suite);
-  assertOutput("starting...\n**E\nend.\n");
-}
 
 } // namespace
