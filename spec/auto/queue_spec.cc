a01987957c88f1073403f5485442969d67a05ac7
horance <horance@outlook.com>
2019-09-19 20:04:01 +0800

    self test using stackspec and queuespec

---
+++ spec/auto/queue_spec.cc
@@ -0,0 +1,34 @@
+#include "cctest/cctest.h"
+#include <queue>
+
+using namespace cctest;
+
+namespace {
+
+FIXTURE(QueueSpec) {
+  std::queue<int> q;
+
+  SETUP {
+    q.push(1);
+    q.push(2);
+  }
+
+  TEST("apply pop 0 time") {
+    ASSERT_EQ(1, q.front());
+    ASSERT_EQ(2, q.back());
+  }
+
+  TEST("apply pop 1 time") {
+    q.pop();
+    ASSERT_EQ(2, q.front());
+    ASSERT_EQ(2, q.back());
+  }
+
+  TEST("apply pop 2 times") {
+    q.pop();
+    q.pop();
+    ASSERT_TRUE(q.empty());
+  }
+};
+
+} // namespace
---
+++ spec/auto/stack_spec.cc
@@ -0,0 +1,32 @@
+#include "cctest/cctest.h"
+#include <stack>
+
+using namespace cctest;
+
+namespace {
+
+FIXTURE(StackSpec) {
+  std::stack<int> v;
+
+  SETUP {
+    v.push(1);
+    v.push(2);
+  }
+
+  TEST("apply pop 0 time") {
+    ASSERT_EQ(2, v.top());
+  }
+
+  TEST("apply pop 1 time") {
+    v.pop();
+    ASSERT_EQ(1, v.top());
+  }
+
+  TEST("apply pop 2 times") {
+    v.pop();
+    v.pop();
+    ASSERT_TRUE(v.empty());
+  }
+};
+
+} // namespace
